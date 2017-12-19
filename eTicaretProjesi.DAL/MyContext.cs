012e700851126f68d9550b7788d10f36debe2ad6
CevatAYDN <cvtaydn53@gmail.com>
2017-12-19 14:11:45 +0300

    Migrations Yapıldı, Başvurular Eklendi.

---
+++ eTicaretProjesi.DAL/MyContext.cs
@@ -0,0 +1,31 @@
+﻿using eTicaretProjesi.ENT.IdentityModel;
+using eTicaretProjesi.ENT.Model;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTicaretProjesi.DAL
+{
+    public class MyContext : IdentityDbContext<AppUser>
+    {
+        public MyContext()
+            : base("name=MyCon")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            this.RequireUniqueEmail = true;
+        }
+
+        public virtual DbSet<Urunler> Urunler { get; set; }
+        public virtual DbSet<KategoriAlt> KategoriAlt { get; set; }
+        public virtual DbSet<Kategoriler> Kategoriler { get; set; }
+    }
+}
