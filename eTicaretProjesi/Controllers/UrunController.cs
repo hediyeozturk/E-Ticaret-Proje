a9ea258866e6c23e5f423f1a4e1dd749955c91cd
mesutozturkk <mstztrk46@gmail.com>
2017-12-25 18:46:48 +0300

    urunController

--- eTicaretProjesi/Controllers/UrunController.cs
+++ eTicaretProjesi/Controllers/UrunController.cs
@@ -11,6 +11,7 @@ namespace eTicaretProjesi.Controllers
     public class UrunController : Controller
     {
         KategoriRepo kr = new KategoriRepo();
+        UrunlerRepo ur = new UrunlerRepo();
         AltKategoriRepo akr = new AltKategoriRepo();
         // GET: Urun
         public ActionResult KategoriGetir()
@@ -25,6 +26,24 @@ namespace eTicaretProjesi.Controllers
             }).ToList();
             
             return Json(klist, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult UrunGetir()
+        {
+            var ulist = ur.GenelListele().Select(x => new {
+                x.UrunAD,
+                x.UrunID,
+                x.UrunFiyat,
+                x.UrunAciklama
+            }).ToList();
+
+            return Json(ulist, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Urunler()
+        {
+            UrunlerViewModel model = new UrunlerViewModel();
+            model.UrunlerList = ur.Listele();
+            return View(model);
         }
     }
 }
